966dd696bc6422968363613c6ffa5ba56061ae7f
Narensham <naren.sham1990@gmail.com>
2025-10-19 21:25:55 -0400

    Remove venv and build artifacts from git tracking

--- dbt/target/compiled/buildcpg/models/marts/sales_forecast_ready.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-  sku_id,
-  region,
-  DATE_TRUNC('week', order_date) AS week,
-  SUM(sales) AS total_sales,
-  AVG(discount) AS avg_discount,
-  AVG(stock_on_hand) AS avg_stock
-FROM "buildcpg"."main"."stg_sales"
-GROUP BY 1,2,3
--- dbt/target/compiled/buildcpg/models/staging/bronze_sales.sql
+++
@@ -1,2 +0,0 @@
--- Bronze: read raw CSV with DuckDB's read_csv_auto
-SELECT * FROM read_csv_auto('../data/raw/sales_2025_09.csv')  -- dbt-duckdb supports read_csv_auto
