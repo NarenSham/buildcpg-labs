604ab5abfb0bb23e3af20b469e9d2121ccb8a2c5
Narensham <naren.sham1990@gmail.com>
2025-11-09 13:37:00 -0500

    Move timestamps outside the DISTINCT

--- lab2_market_sentiment/dbt/models/dimensions/dim_brands.sql
+++ lab2_market_sentiment/dbt/models/dimensions/dim_brands.sql
@@ -5,20 +5,25 @@
 
 WITH brand_data AS (
     SELECT DISTINCT
-        {{ generate_surrogate_key(['brand']) }} as brand_key,
         brand,
-        parent_company,                                    -- ✅ Now comes from data
-        brand_category as category,                        -- ✅ Now comes from data
+        parent_company,
+        brand_category as category,
         CASE 
             WHEN parent_company IN ('The Coca-Cola Company', 'PepsiCo Inc.', 'Unilever PLC', 
                                     'The Procter & Gamble Company', 'Nestlé S.A.') 
             THEN 'Public'
             ELSE 'Private'
-        END as company_type,
-        TRUE as is_active,
-        get_current_timestamp() as _dbt_created_at,
-        '{{ run_started_at }}' as _dbt_run_timestamp
+        END as company_type
     FROM {{ ref('int_sentiment_unified') }}
 )
 
-SELECT * FROM brand_data
+SELECT
+    {{ generate_surrogate_key(['brand']) }} as brand_key,
+    brand,
+    parent_company,
+    category,
+    company_type,
+    TRUE as is_active,
+    get_current_timestamp() as _dbt_created_at,
+    '{{ run_started_at }}' as _dbt_run_timestamp
+FROM brand_data
