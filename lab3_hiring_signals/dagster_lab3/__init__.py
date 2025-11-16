914b62d0f3d288e077654b8bfa3aea0030802618
Narensham <naren.sham1990@gmail.com>
2025-11-16 17:28:20 -0500

    Remove lab3_hiring_signals (moved to its own repo)

--- lab3_hiring_signals/dagster_lab3/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Lab 3: Hiring Signals Pipeline - Dagster orchestration."""
-
-__version__ = "0.1.0"
--- lab3_hiring_signals/dagster_lab3/assets/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Dagster assets for Lab 3 hiring signals pipeline."""
-
-from .raw_jobs import raw_jobs_asset
-
-__all__ = ["raw_jobs_asset"]
